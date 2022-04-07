554c03181b403ef326b962819cc5230ade7b38ab
saburkev <100319544+saburkev@users.noreply.github.com>
2022-04-07 14:58:30 -0500

    if button pressed, move servo

--- First_code/First_code.ino
+++ First_code/First_code.ino
@@ -15,11 +15,17 @@ const int buttoonPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
-myServo.attach(9);
+myServo.attach(9); //using .attach to initialize servo to pin 9
 pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //if button is pressed, swivel servo 180 degrees
+  if(digitalRead(buttonPin) == HIGH) {
+    myServo.write(180);
+  } else {
+    myServo.write(0); 
+  }
 
 }
