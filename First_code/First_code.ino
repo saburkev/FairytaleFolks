e1e9c20502c59bc34cc9704a50c9bd8064eef8ea
saburkev <100319544+saburkev@users.noreply.github.com>
2022-04-07 14:53:10 -0500

    Initialized servo and button pins

--- First_code/First_code.ino
+++ First_code/First_code.ino
@@ -1,6 +1,22 @@
+/*
+AET Mulan Enclosure
+Team Name: Fairytale Folks
+Team Members: Kelly Zhang & Schuyler Burke
+Description: button and servo program
+*/
+
+//Servo code library
+#include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttoonPin = 7;
+
+
 void setup() {
   // put your setup code here, to run once:
-
+myServo.attach(9);
+pinMode(buttonPin, INPUT);
 }
 
 void loop() {
