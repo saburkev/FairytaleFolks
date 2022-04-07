d5d978e00d94be3caa25bd0621458b85b48ce596
kekethekiwi <89766733+kekethekiwi@users.noreply.github.com>
2022-04-07 14:54:24 -0500

    intialize servo and button pin

--- First_code/First_code.ino
+++ First_code/First_code.ino
@@ -1,6 +1,21 @@
+/*
+AET Mulan Enclosure
+Team Name: FairyTale Folks 
+Team Members: Schuyler and Kelly
+Description: button and servo program
+*/
+
+// Servo Code Library
+#include<Servo.h>;
+
+// Variables
+Servo myServo;
+const int buttonPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT)
 }
 
 void loop() {
