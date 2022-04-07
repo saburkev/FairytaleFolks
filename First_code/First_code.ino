c5e6c2aecd80d67a9f37eb4ffb11edcc48683c0f
kekethekiwi <89766733+kekethekiwi@users.noreply.github.com>
2022-04-07 14:58:16 -0500

    if button pressed, swivel servo

--- First_code/First_code.ino
+++ First_code/First_code.ino
@@ -14,11 +14,17 @@ const int buttonPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
-  myServo.attach(9);
-  pinMode(buttonPin, INPUT)
+  myServo.attach(9); // using .attach to initialize servo to pin 9
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // if button i spressed, swivel servo 180 degrees
+  if (digitalRead(buttonPin) == HIGH) {
+    myServo.write(180);
 
+  } else {
+    myServo.write(0);
+  }
 }
